ee5f26de832a148551696624448f6a63ffe8b665
Harshal <harshaltapre27@gmail.com>
2026-01-28 18:45:58 +0530

    Update import statement in db-utils to include file extension for consistency and compatibility.

--- api/db-utils.ts
+++ api/db-utils.ts
@@ -5,7 +5,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import * as schema from "@shared/schema";
+import * as schema from "../shared/schema.js";
 
 const { Pool } = pg;
 
